2201540963864cf7ced1d0f79266d0b59225ef4d
Skald98 <49116546+Skald98@users.noreply.github.com>
2019-03-31 20:10:01 +0300

    Add files via upload

---
+++ JavaExercise7.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaexercise7;
+
+/**
+ *
+ * @author Skald
+ */
+public class JavaExercise7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    
+    
+    //We can use the same methods and classes with Scanner as well.
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Item i=new Item();
+   Customer j=new Customer();
+        
+    }
+    
+}
+
+class Item{
+    
+    String[] ItemName=Iteminfo();
+   
+ public String[] Iteminfo(){
+     
+     String []Itemname={"Tea Packages","Bread"};
+      int []ItemQuantity={4,3};
+     int [] payments={3,2};
+        int [] Amounts_still_due={2,3};
+     
+      System.out.println("Item name: "+Itemname[0]+", Item quantity: "+ItemQuantity[0]+", Item Payments: "+payments[0]+", Amounts due: "+Amounts_still_due[0]);
+       System.out.println("Item name: "+Itemname[1]+", Item quantity: "+ItemQuantity[1]+", Item Payments: "+payments[1]+", Amounts due: "+Amounts_still_due[1]);
+     return Itemname;
+ }
+    
+}
+
+class Customer{
+    String []Customer=customerinfo();
+
+     public String[] customerinfo(){
+         
+        String []BillingAddress={"Lamia"};
+        System.out.println("Billing Addresses: "+BillingAddress[0]);
+           String []ShippingAddress={"Lamia, Town Square"};
+               System.out.println("Shipping Addresses: "+ShippingAddress[0]);
+     
+               return BillingAddress;
+     }
+
+    }
+    
+    
+    
+    
+
